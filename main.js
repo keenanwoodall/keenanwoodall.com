9ec85e84a1a59874f8b2c61952a85d033c70f274
Keenan Woodall <keenanwoodall@gmail.com>
2018-06-07 13:28:59 -0500

    Got perfect video sizing.

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 window.onload = onLoad;
+window.onresize = resizeVideos;
 
 function onLoad ()
 {
-    fadeOutOverlay ();
+    cacheAspectRatio ();
+    resizeVideos ();
+    disableOverlay ();
 }
 
-function fadeOutOverlay ()
+function cacheAspectRatio ()
+{
+    var videos = document.getElementsByTagName ("iframe");
+    for (var i = 0; i < videos.length; i++)
+    {
+        var element = videos[i];
+        var aspectRatio = element.height / element.width;
+        element.removeAttribute ("width");
+        element.removeAttribute ("height");
+        element.setAttribute ("aspectratio", aspectRatio);
+    }
+}
+
+function resizeVideos ()
+{
+    var newWidth = document.body.clientWidth;
+
+    var videos = document.getElementsByTagName ("iframe");
+    for (var i = 0; i < videos.length; i++)
+    {
+        var element = videos[i];
+        var aspectRatio = element.getAttribute ("aspectratio");
+
+
+        element.setAttribute ("width", (newWidth * 0.8));
+        element.setAttribute ("height", (newWidth * 0.8) * aspectRatio);
+    }
+}
+
+function disableOverlay ()
 {
     setTimeout (function ()
         {   
